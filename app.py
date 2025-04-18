281488e980380ac37172870a2680d806c763692e
Aedla Varshit <varshitreddy27914@gmail.com>
2025-04-18 09:55:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import threading
 from olwik import run_bot  # Import the bot function
+import os
+
 
 app = Flask(__name__)
 
@@ -12,3 +14,6 @@ def index():
 def start():
     threading.Thread(target=run_bot).start()
     return "Olwik started!"
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
